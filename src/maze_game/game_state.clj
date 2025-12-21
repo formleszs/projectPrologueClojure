2fa05dd756f3bb6c33ff7fad3bd2ef0e5c567041
Ilya Kramarenko <ilyakkr18@mail.ru>
2025-12-21 18:05:36 +0300

    changes in update-bot func and added parse-path

--- src/maze_game/game_state.clj
+++ src/maze_game/game_state.clj
@@ -17,7 +17,8 @@
 (def prolog-process (atom nil))
 
 (defn call-prolog [query]
-  (let [result (shell/sh "swipl" "-q" "-f" "src/prolog/maze_logic.pl"
+  (let [result (shell/sh "swipl" "-q"
+                         "-f" "src/prolog/maze_logic.pl"
                          "-f" "src/prolog/pathfinding.pl"
                          "-g" query "-t" "halt")]
     (if (= 0 (:exit result))
@@ -25,17 +26,20 @@
       (throw (Exception. (str "Prolog error: " (:err result)))))))
 
 (defn initialize-game []
-  (reset! game-state {
-    :players {}
-    :bot {:x 0 :y 0 :target nil}
-    :maze {
-      :walls #{[0 0] [1 0] [2 0] [0 1] [2 1] [0 2] [1 2] [2 2]}
-      :exit [1 1]
-      :size [3 3]
-    }
-    :game-status :playing
-    :tick 0
-  }))
+  (reset! game-state
+    {:players {}
+     :bot {:x 0 :y 0 :target nil}
+     :maze {:walls #{[0 0] [2 0] [3 0] [4 0]
+                    [0 1]               [4 1]
+                    [0 2] [1 2] [2 2]   [4 2]
+                    [0 3]               [4 3]
+                    [0 4] [2 4]         [4 4]
+                    [0 5] [2 5]         [4 5]
+                    [0 6] [1 6] [2 6] [3 6] [4 6]}
+           :exit [1 0]
+           :size [5 7]}
+     :game-status :playing
+     :tick 0}))
 
 (defn add-player [player-id]
   (swap! game-state update-in [:players player-id]{
@@ -62,30 +66,38 @@
       (swap! game-state assoc-in [:players player-id :x] new-x)
       (swap! game-state assoc-in [:players player-id :y] new-y))))
 
+(defn parse-path [s]
+  (mapv (fn [[_ xs ys]]
+          [(Integer/parseInt xs) (Integer/parseInt ys)])
+        (re-seq #"\((\d+),\s*(\d+)\)" s)))
+
 (defn update-bot []
   (let [bot (:bot @game-state)
         players (:players @game-state)
-        alive-players (filter #(= :alive (:status (val %))) players)]
+        alive-players (filter (fn [[_ p]] (= :alive (:status p))) players)]
     (when (seq alive-players)
-      (let [target (first alive-players)
-            [player-id player] target
-            path (call-prolog (str "find_path(" (:x bot) "," (:y bot)
-                                   "," (:x player) "," (:y player) ", Path)."))]
-        (when-let [next-pos (second (re-find #"\[\((\d+),(\d+)\)" path))]
-          (let [[x y] (map #(Integer/parseInt %) (str/split next-pos #","))]
+      (let [[player-id player] (first alive-players)
+            query (format "find_path(%d,%d,%d,%d,Path), writeln(Path)."
+                          (:x bot) (:y bot) (:x player) (:y player))
+            path-str (call-prolog query)
+            path (parse-path path-str)
+            next-pos (second path)]
+        (when next-pos
+          (let [[x y] next-pos]
             (swap! game-state assoc :bot {:x x :y y :target player-id})
 
-            ; Проверка столкновения
-            (doseq [[pid p] players]
-              (when (and (= (:x p) x) (= (:y p) y) (= (:status p) :alive))
+            (doseq [[pid p] (:players @game-state)]
+              (when (and (= (:x p) x)
+                         (= (:y p) y)
+                         (= (:status p) :alive))
                 (swap! game-state assoc-in [:players pid :status] :dead)))))))))
+
 
 (defn game-tick []
   (when (= :playing (:game-status @game-state))
     (swap! game-state update :tick inc)
     (update-bot)
 
-    ; Проверка условий окончания игры
     (let [players (:players @game-state)
           alive-count (count (filter #(= :alive (:status (val %))) players))
           exit (:exit (:maze @game-state))
