f71a522cd37ac4f550d9dd17ebe41ce6f7cfe6e2
Ilya Kramarenko <ilyakkr18@mail.ru>
2025-12-21 17:04:17 +0300

    changed libs versions, move files

--- project.clj
+++ project.clj
@@ -2,9 +2,9 @@
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [http-kit "2.6.0"]
                  [org.clojure/data.json "2.4.0"]
-                 [seancorfield/next.jdbc "1.3.874"]
+                 [com.github.seancorfield/next.jdbc "1.3.874"]
                  [com.layerware/hugsql "0.5.3"]
-                 [tupelo "23.08.09"]
+                 [tupelo "25.07.04"]
                  [org.slf4j/slf4j-simple "2.0.7"]]
   :main maze-game.core
   :profiles {:dev {:dependencies [[nrepl "1.0.0"]]}})
