26d6505e14cd15852b6df9db0943256117bc493b
Chezem141 <p.ermolaew2004@mail.ru>
2025-12-21 19:39:58 +0300

    Remake pathfinding.pl

--- src/prolog/pathfinding.pl
+++ src/prolog/pathfinding.pl
@@ -1,35 +1,40 @@
 find_path(StartX, StartY, EndX, EndY, Path) :-
-    find_path_bfs([(StartX, StartY)], EndX, EndY, [(StartX, StartY)], Path).
+    bfs([[(StartX, StartY)]], (EndX, EndY), [(StartX, StartY)], RevPath),
+    reverse(RevPath, Path).
 
-find_path_bfs([], _, _, _, _) :- 
+bfs([], _, _, _) :-
     fail.
 
-find_path_bfs([(EndX, EndY)|_], EndX, EndY, Visited, Path) :-
-    reverse(Visited, Path).
+bfs([[Goal|Rest]|_], Goal, _, [Goal|Rest]) :- !.
 
-find_path_bfs([(X,Y)|Rest], EndX, EndY, Visited, Path) :-
-    findall((NX, NY), 
-            (adjacent(X, Y, NX, NY), 
-             valid_move(NX, NY),
-             \+ member((NX, NY), Visited)),
-            NewCells),
-    append(Rest, NewCells, NewQueue),
-    append(Visited, NewCells, NewVisited),
-    find_path_bfs(NewQueue, EndX, EndY, NewVisited, Path).
+bfs([[(X,Y)|RestPath] | OtherPaths], Goal, Visited, FoundPath) :-
+    findall([(NX,NY),(X,Y)|RestPath],
+            ( adjacent((X,Y),(NX,NY)),
+              valid_move(NX, NY),
+              \+ member((NX,NY), Visited)
+            ),
+            NewPaths),
+    new_cells_only(NewPaths, NewCells),
+    append(Visited, NewCells, Visited1),
+    append(OtherPaths, NewPaths, Queue1),
+    bfs(Queue1, Goal, Visited1, FoundPath).
 
-adjacent(X, Y, X1, Y) :- X1 is X + 1.
-adjacent(X, Y, X1, Y) :- X1 is X - 1.
-adjacent(X, Y, X, Y1) :- Y1 is Y + 1.
-adjacent(X, Y, X, Y1) :- Y1 is Y - 1.
+new_cells_only([], []).
+new_cells_only([ [Cell|_] | Paths], [Cell|Cells]) :-
+    new_cells_only(Paths, Cells).
 
-% Найти ближайшего игрока для бота
+adjacent((X,Y), (X1,Y)) :- X1 is X + 1.
+adjacent((X,Y), (X1,Y)) :- X1 is X - 1.
+adjacent((X,Y), (X,Y1)) :- Y1 is Y + 1.
+adjacent((X,Y), (X,Y1)) :- Y1 is Y - 1.
+
 find_nearest_player(BotX, BotY, PlayerX, PlayerY) :-
-    player_position(PlayerId, PlayerX, PlayerY),
     findall(Distance-PlayerId-PX-PY,
-            (player_position(PId, PX, PY),
-             distance(BotX, BotY, PX, PY, Distance)),
+            ( player_position(PlayerId, PX, PY),
+              distance(BotX, BotY, PX, PY, Distance)
+            ),
             Distances),
-    sort(Distances, [_-PlayerId-PlayerX-PlayerY|_]).
+    sort(Distances, [_-_-PlayerX-PlayerY|_]).
 
 distance(X1, Y1, X2, Y2, D) :-
     DX is X2 - X1,
